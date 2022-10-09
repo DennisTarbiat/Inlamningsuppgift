00cc81c2f4cf202a2b39f6dfa8fcb2149f196d97
DennisTarbiat <dennis.tarbiat@gmail.com>
2022-10-09 17:40:22 +0200

    Added function to fetch todos

--- www/script.js
+++ www/script.js
@@ -1,1 +1,9 @@
+function getTodos() {
+  fetch("../Data/todo.json")
+    .then((respone) => (respone = respone.json()))
+    .then((data) => {
+      console.log(data.todoList);
+    });
+}
 
+getTodos();
