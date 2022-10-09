212a66dbaae2547d33812c20b2fab044ff5aaa42
DennisTarbiat <dennis.tarbiat@gmail.com>
2022-10-10 00:19:27 +0200

    Added JSdocs to function

--- www/getTodos.js
+++ www/getTodos.js
@@ -1,3 +1,7 @@
+/**
+ * Fetches the todos from local file
+ * @returns {Promise<any[]>}
+ */
 export async function getTodos() {
   const response = await fetch("../data/todoList.json");
   const data = await response.json();
