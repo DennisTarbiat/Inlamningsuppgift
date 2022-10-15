e0df3f4c207d0ff619be632cd50e492ba3a8488e
DennisTarbiat <dennis.tarbiat@gmail.com>
2022-10-15 02:54:15 +0200

    Comment changes

--- www/appendLists.js
+++ www/appendLists.js
@@ -4,17 +4,17 @@
  * @param {any[]} uncompletedTodos
  */
 export function appendLists(completedTodos, uncompletedTodos) {
-  //Creates and appends a "h1" with description of the users options
+  //Creates and appends a "h1" element with description of the users options
   const pageDescription = document.createElement("h1");
   pageDescription.textContent =
     "Klicka på vilken uppgift i listan du vill flytta på!";
   document.body.appendChild(pageDescription);
 
-  //Creates and append "div" for completed list
+  //Creates and appends a "div" element for completed list
   const completedDiv = document.createElement("div");
   document.body.appendChild(completedDiv);
-  //Creates and appends a "ul" for the completed list and sets it "id" to "completed"
-  //Also creates and appends a title for the "ul"
+  //Creates and appends a "ul" element for the completed list and sets it "id" to "completed"
+  //Also creates and appends a title for the "ul" element
   const completedUl = document.createElement("ul");
   const completedTitle = document.createElement("h2");
   completedTitle.textContent = "Klara Uppgifter";
@@ -22,11 +22,11 @@ export function appendLists(completedTodos, uncompletedTodos) {
   completedDiv.appendChild(completedTitle);
   completedDiv.appendChild(completedUl);
 
-  //Creates and append "div" for uncompleted list
+  //Creates and appends a "div" element for uncompleted list
   const uncompletedDiv = document.createElement("div");
   document.body.appendChild(uncompletedDiv);
-  //Creates and append a "ul" for the uncompleted list and sets it "id" to "uncompleted"
-  //Also creates and appends a title for the "ul"
+  //Creates and append a "ul" element for the uncompleted list and sets it "id" to "uncompleted"
+  //Also creates and appends a title for the "ul" element
   const uncompletedUl = document.createElement("ul");
   const uncomplitedTitle = document.createElement("h2");
   uncomplitedTitle.textContent = "Oklara Upggifter";
@@ -34,27 +34,27 @@ export function appendLists(completedTodos, uncompletedTodos) {
   uncompletedDiv.appendChild(uncomplitedTitle);
   uncompletedDiv.appendChild(uncompletedUl);
 
-  //Creates and append a "li" for each element in "completedTodos" array
+  //Creates and appends a "li" element for each element in the "completedTodos" array
   for (const todo of completedTodos) {
     const li = document.createElement("li");
     li.textContent = todo.title;
-    //Changes the "ul" when clicking on the "li"
+    //Changes list when clicking on the "li" element
     li.addEventListener("click", toggleList);
     completedUl.appendChild(li);
   }
 
-  //Creates and append a "li" for each element in "uncompletedTodos" array
+  //Creates and appends a "li" element for each element in the "uncompletedTodos" array
   for (const todo of uncompletedTodos) {
     const li = document.createElement("li");
     li.textContent = todo.title;
-    //Changes the "ul" when clicking on the "li"
+    //Changes list when clicking on the "li" element
     li.addEventListener("click", toggleList);
     uncompletedUl.appendChild(li);
   }
 }
 
 /**
- * Appends the "li" to uncompleted or completed "ul" depending on "parentNode id"
+ * Appends the "li" element to uncompleted or completed "ul" element depending on the "parentNode id"
  * @returns {void}
  */
 function toggleList() {
